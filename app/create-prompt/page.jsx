f751fcf150f42bfc88371d61429bc86827dc08f5
Maaz0506 <121034679+Maaz0506@users.noreply.github.com>
2024-03-11 13:20:14 +0500

    create-page done

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,14 +6,46 @@ import { useRouter } from "next/router";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
-  return <Form />;
+  const createPrompt = async (e) => {
+    e.preventDDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
